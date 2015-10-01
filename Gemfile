9be9c17b155b59eec70dbedf578ff1d34c266259
Mirco Cotugno <mirco@pitchtarget.com>
2015-10-01 15:20:02 +0200

    create deploy

--- Gemfile
+++ Gemfile
@@ -20,3 +20,4 @@ gem 'therubyracer'
 
 #Bower
 gem "middleman-bower"
+gem 'middleman-deploy', '~> 1.0'
--- config.rb
+++ config.rb
@@ -75,3 +75,12 @@ end
 after_configuration do
   sprockets.append_path File.join root.to_s, "bower_components"
 end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
