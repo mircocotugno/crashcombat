7a2edb1d1427a114effa9108ec7ed3c4053080db
mirco@pitchtarget.com <mirco@pitchtarget.com>
2015-10-05 09:58:23 +0200

    Automated commit at 2015-10-05 07:58:20 UTC by middleman-deploy 1.0.0

---
+++ javascripts/360-carousel.js
@@ -0,0 +1,25 @@
+window.onload = init;
+
+var product;
+function init(){
+    var $slider = $('.slider-360');
+    var dimension = $(window).height();
+    var ninetypc = (100 * dimension) / 100;
+
+    $slider.ThreeSixty({
+        totalFrames: 72, // Total no. of image you have for 360 slider
+        endFrame: 72, // end frame for the auto spin animation
+        currentFrame: 52, // This the start frame for auto spin
+        imgList: '.threesixty_images', // selector for image list
+        progress: '.spinner', // selector to show the loading progress
+        imagePath: $slider.data('images'), // path of the image assets
+        filePrefix: '', // file prefix if any
+        ext: '.png', // extention for the assets
+        height: ninetypc,
+        width: ('100%'),
+        navigation: false,
+        disableSpin: false // Default false
+    });
+
+}
+;
